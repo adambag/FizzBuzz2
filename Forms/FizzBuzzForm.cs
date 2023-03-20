19870ae5b1ae0d62ade1a80326015edbc682424e
Adam Bagiński <abaginski2001@gmail.com>
2023-03-20 23:43:18 +0100

    Dodaj pliki projektów.

---
+++ Forms/FizzBuzzForm.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Xml.Linq;
+using System.ComponentModel.DataAnnotations;
+
+
+namespace FizzBuzz.Forms
+{
+    public class FizzBuzzForm
+    {
+       
+        [Display(Name = "Twój szczęśliwy numerek")]
+        [Required][Range(1, 1000, ErrorMessage = "Oczekiwana wartość {0} z zakredu {1} i {2}.")]
+        public int Number { get; set; }
+
+
+    }
+}
---
+++ Pages/Index.cshtml.cs
@@ -0,0 +1,63 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using FizzBuzz.Forms;
+
+namespace FizzBuzz.Pages
+{
+    public class IndexModel : PageModel
+    {
+        private readonly ILogger<IndexModel> _logger;
+        [BindProperty]
+
+        public FizzBuzzForm FizzBuzz { get; set; }
+        [BindProperty(SupportsGet = true)]
+        public string Name { get; set; }
+
+        public string Result { get; set; }
+
+        public IndexModel(ILogger<IndexModel> logger)
+        {
+            _logger = logger;
+        }
+        public IActionResult OnPost()
+        {
+            if (!ModelState.IsValid)
+            {
+                IfDivided(FizzBuzz.Number);
+                return Page();
+            }           
+            return RedirectToPage("./Privacy");
+        }
+        public void IfDivided(int number)
+        {
+            if (number % 3 == 0 && number % 5 == 0)
+            {
+                Result = "FizzBuzz";
+            }
+            else if (number % 3 == 0)
+            {
+                Result =  "Fizz";
+            }
+            else if (number % 5 == 0)
+            {
+                Result = "Buzz";
+            }
+           
+            else
+            {
+                Result = "Liczba " + number + " nie spełnia warunkow FizzBuzz";
+
+            }
+        }
+
+
+        public void OnGet()
+        {
+            if (string.IsNullOrWhiteSpace(Name))
+            {
+                Name = "User";
+            }
+
+        }
+    }
+}
